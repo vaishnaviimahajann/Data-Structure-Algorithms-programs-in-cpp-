14b0c8e45c9d2abd5672e2e64948ebc055c1a542
vaishnaviimahajann <vaishnavimahajann.in>
2026-01-03 17:00:44 +0530

    some chnages

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -1,27 +1,32 @@
 #include<iostream>
 using namespace std;
-void bubblesort(int *arr,int n){
+void bbubblesort(int *arr,int n){
 
-    int flag =0;
+    
     for(int i=0;i<n;i++){
+        int flag =0;
         for (int j=0;j<n-i-1;j++){
       if (arr[j]>arr[j+1]){
        int temp=arr[j];
         arr[j]=arr[j+1];
         arr[j+1]=temp;
-        flag=1
+         flag=1;
       }
     }
     if (flag==0){
-        break;
+        break; 
     }
-    cout<<"sorted array is: ";
+   
+    
+     cout<<"sorted array is: ";
     for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
-    }
+        cout<<arr[i]<<" "<<endl;
+}
+
+}
 }
 int main(){
-    int arr[]={23,56,75,1,89,99};
-    bubblesort(arr,6);
+    int arr[]={3,76,2,12,45,64};
+    bbubblesort(arr,6);
     return 0;
 }
