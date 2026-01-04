50e1c082c0d101a1ee7c402c82b788b87becc7b7
vaishnaviimahajann <vaishnavimahajann.in>
2026-01-04 13:14:16 +0530

    optimized insertion sort

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -1,29 +1,30 @@
 #include<iostream>
 using namespace std;
-void insertionsort(int*arr,int n){
-    for(int i=0;i<n;i++){
-        int curr=arr[i];
-        int prev=i-1;
-        while(prev>0&&prev>curr)
+
+void insertionsort(int* arr, int n)
+{
+    for(int i = 1; i < n; i++)
+    {
+        int curr = arr[i];
+        int prev = i - 1;
+
+        while(prev >= 0 && arr[prev] > curr)
         {
-            int temp=arr[prev+1];
-            arr[prev+1]=arr[prev];
-            arr[prev]=temp;
+            arr[prev + 1] = arr[prev]; 
             prev--;
         }
-        arr[prev+1]=curr;
-        
-        cout<<"sorted array is: ";
-        for(int i=0;i<n;i++){
-            cout<<arr[i]<<" ";
-        }
-        cout<<endl;
+        arr[prev + 1] = curr;
     }
 
+    cout << "sorted array is: ";
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
 }
-int main(){
-    int arr[5]={5,4,3,2,1};
-    insertionsort(arr,5);
 
+int main()
+{
+    int arr[10] = {45,76,43,2,3,56,67,87,1,2};
+    insertionsort(arr,10);
     return 0;
 }
